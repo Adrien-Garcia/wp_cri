de6e967c97605ecf5d5acbe59e2a767a4cde2047
Jean-François Besson <jf.besson@addonline.fr>
2015-01-28 17:01:20 +0100

    - ajout de la fonction hide_email()
    Signed-off-by: Jean-François Besson <jf.besson@addonline.fr>

--- server/wp-content/themes/maestro/functions.php
+++ server/wp-content/themes/maestro/functions.php
@@ -212,4 +212,20 @@ function bones_fonts() {
 
 add_action('wp_print_styles', 'bones_fonts');
 
+/*
+ * HIDE EMAIL ADDRESS
+* faire un "echo hide_email(adresse@domaine.tld)" pour crypter l'adresse email en front
+* */
+function hide_email($email) {
+    $character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+    $key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
+    for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
+    $script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+    $script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+    $script.= 'document.getElementById("'.$id.'").innerHTML="<a itemprop=\\"email\\" class=\\"contact_field\\" href=\\"mailto:"+d+"\\">"+d+"</a>"';
+    $script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")";
+    $script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
+    return '<span id="'.$id.'">[javascript protected email address]</span>'.$script;
+}
+
 ?>
