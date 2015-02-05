3acba8f3369389419abde140ff99955f53194f65
Ferreira Laurent <laurent.ferreira@jetpulp.fr>
2015-02-05 11:12:11 +0100

    Suppression des balises script du template pour l'option GA

--- server/wp-content/themes/maestro/content-ga.php
+++ server/wp-content/themes/maestro/content-ga.php
@@ -6,8 +6,6 @@ $options = get_option( 'theme_settings' );
 // check if the enable tracking is on, then show it
 if ($options['enable_tracking'] == true) : ?>
 
-	<script>
-		<?php echo stripslashes($options['tracking']); ?>
-	</script>
+	<?php echo stripslashes($options['tracking']); ?>
 	
 <?php endif; ?>
