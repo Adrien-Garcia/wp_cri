f9c14eee556cbfa391f057e07dd6856211793737
Ferreira Laurent <laurent.ferreira@jetpulp.fr>
2015-02-06 16:29:31 +0100

    Configuration

--- sql/env.sql
+++ sql/env.sql
@@ -9,8 +9,6 @@ UPDATE wp_posts SET post_content = replace(post_content, 'http://www.ancien-site
 
 UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://www.ancien-site.com', 'http://www.nouveau-site.com');
 
-
-
 /* Sous domaines (images & static) */
 UPDATE wp_options set option_value = "http://images.nouveau-site.com/wp-content/uploads" where option_name = "upload_url_path";
 UPDATE wp_posts SET post_content = REPLACE(post_content,'http://www.ancien-site.com/wp-content/uploads','http://images.nouveau-site.com/wp-content/uploads')
