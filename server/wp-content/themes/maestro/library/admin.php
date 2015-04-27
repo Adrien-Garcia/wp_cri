0c43908b89959f1a797a7c8c44febeef6fc38041
Ferreira Laurent <laurent.ferreira@jetpulp.fr>
2015-04-27 14:49:24 +0200

    config auto update

--- server/wp-content/themes/maestro/library/admin.php
+++ server/wp-content/themes/maestro/library/admin.php
@@ -23,8 +23,7 @@ Digging into WP - http://digwp.com/2010/10/customize-wordpress-dashboard/
 */
 
 // Manage upadtes
-define('WP_AUTO_UPDATE_CORE', true);
-add_filter( 'auto_update_plugin', '__return_true' );
+define('WP_AUTO_UPDATE_CORE', 'minor');
 
 
 /************* DASHBOARD WIDGETS *****************/
--- sql/env.sql
+++ sql/env.sql
@@ -10,7 +10,6 @@ UPDATE wp_posts SET guid = replace(guid, 'http://www.ancien-site.com','http://ww
 
 # Copier / Remplacer contenu des postes
 UPDATE wp_posts SET post_content = replace(post_content, 'http://www.ancien-site.com', 'http://www.nouveau-site.com');
-
 UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://www.ancien-site.com', 'http://www.nouveau-site.com');
 
 /* Sous domaines (images & static) */
