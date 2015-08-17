a3ceb51e78b9b0bd470f269b4e598171dc63c60c
Ferreira Laurent <laurent.ferreira@jetpulp.fr>
2015-08-17 09:29:35 +0200

    Configuration vagrant-admin

--- server/wp-config.php
+++ server/wp-config.php
@@ -6,7 +6,7 @@ define('WP_CACHE', false); // Added by WP Rocket
 *
 * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
 * préfixe de table, clefs secrètes, langue utilisée, et ABSPATH.
-* Vous pouvez en savoir plus à leur sujet en allant sur 
+* Vous pouvez en savoir plus à leur sujet en allant sur
 * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
 * wp-config.php}. C'est votre hébergeur qui doit vous donner vos
 * codes MySQL.
@@ -22,22 +22,22 @@ define('WP_CACHE', false); // Added by WP Rocket
 /** Nom de la base de données de WordPress. */
 define('DB_NAME', 'wp_maestro');
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'wp');
+define('DB_USER', 'root');
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'wp');
+define('DB_PASSWORD', 'root');
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
-/** Type de collation de la base de données. 
-* N'y touchez que si vous savez ce que vous faites. 
+/** Type de collation de la base de données.
+* N'y touchez que si vous savez ce que vous faites.
 */
 define('DB_COLLATE', '');
 /**#@+
 * Clefs uniques d'authentification et salage.
 *
 * Remplacez les valeurs par défaut par des phrases uniques !
-* Vous pouvez générer des phrases aléatoires en utilisant 
+* Vous pouvez générer des phrases aléatoires en utilisant
 * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
 * Vous pouvez modifier ces phrases à n'importe quel moment, afin d'invalider tous les cookies existants.
 * Cela forcera également tous les utilisateurs à se reconnecter.
@@ -57,7 +57,7 @@ define('NONCE_SALT',       'g p*oJH!f2bSNmJZ diEfM7Et^&GU_-s6`m*TBv5#u,S+)%iA9T5
 * Préfixe de base de données pour les tables de WordPress.
 *
 * Vous pouvez installer plusieurs WordPress sur une seule base de données
-* si vous leur donnez chacune un préfixe unique. 
+* si vous leur donnez chacune un préfixe unique.
 * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
 * Pour changer le nom des tables dans votre BDD : http://wpchannel.com/modifier-prefixe-base-donnees-wordpress-apres-installation/
 */
@@ -71,15 +71,15 @@ $table_prefix  = 'opgrf_';
 * fr_FR.mo dans wp-content/languages, et réglez l'option ci-dessous à "fr_FR".
 */
 define('WPLANG', 'fr_FR');
-/** 
+/**
 * Pour les développeurs : le mode deboguage de WordPress.
-* 
+*
 * En passant la valeur suivante à "true", vous activez l'affichage des
 * notifications d'erreurs pendant votre essais.
 * Il est fortemment recommandé que les développeurs d'extensions et
-* de thèmes se servent de WP_DEBUG dans leur environnement de 
+* de thèmes se servent de WP_DEBUG dans leur environnement de
 * développement.
-*/ 
+*/
 define('WP_DEBUG', false);
 
 /* Sous domaine pour composants de la charte */
