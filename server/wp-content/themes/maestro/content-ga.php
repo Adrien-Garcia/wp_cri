e6597b5dd3094a24a98f8bf71cb260679d43c2d8
Ferreira Laurent <laurent.ferreira@jetpulp.fr>
2015-10-05 13:32:59 +0200

    Correction notice sur content-ga

--- server/wp-content/themes/maestro/content-ga.php
+++ server/wp-content/themes/maestro/content-ga.php
@@ -4,8 +4,8 @@
 $options = get_option( 'theme_settings' );
 
 // check if the enable tracking is on, then show it
-if ($options['enable_tracking'] == true) : ?>
+if (isset($options['enable_tracking']) && $options['enable_tracking'] == true) : ?>
 
 	<?php echo stripslashes($options['tracking']); ?>
-	
+
 <?php endif; ?>
