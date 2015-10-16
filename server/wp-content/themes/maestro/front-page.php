81a06cc69d2c8c87186125a7c8ba8f0c45f0e0c4
Alexandra Petit <alexandra.petit@jetpulp.fr>
2015-10-16 17:36:18 +0200

    css tablette HP

--- server/wp-content/themes/maestro/front-page.php
+++ server/wp-content/themes/maestro/front-page.php
@@ -225,7 +225,7 @@
 			</div>
 		</div>	
 
-		<div class="row_05">
+		<!--div class="row_05">
 			<div id="inner-content" class="wrap cf">
 				<ul id="">
 					<li class="formations">
@@ -242,32 +242,11 @@
 					</li>
 				</ul>
 			</div>
-		</div>
+		</div!-->
 
 
 	
-		<div id="inner-content" class="wrap cf">
-
-			<div id="main" class="cf" role="main">
-
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-					<div id="home_content">
-
-						<?php if ($options['slider'] != "none" && $options['slider'] == "normal") :
-                        
-                            //Normal Slider
-                        	get_template_part("content","slides");
-                        	
-                        endif; ?>
-
-					</div>
-
-				<?php endwhile; endif; ?>
-
-			</div>
-
-		</div>
+		
 
 	</div>
 
