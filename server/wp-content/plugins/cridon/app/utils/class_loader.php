a4fb84c29a3cba6cb9284bab4b8c7defb4ceb966
fmila <f.mila@etechconsulting-mg.com>
2015-10-16 10:19:24 +0300

    Formations : amener les dernières formations triées par date. Veilles Juridiques : apporter les 3 dernières triées par date.

--- server/wp-content/plugins/cridon/app/utils/class_loader.php
+++ server/wp-content/plugins/cridon/app/utils/class_loader.php
@@ -15,6 +15,7 @@ $dir        = dirname( realpath( __FILE__ ) );
 $pluginRoot = dirname( $dir );
 require_once 'Container.php';
 require_once $pluginRoot.'/models/query_builder.php';
+require_once 'CridonTools.php';
 require_once 'functions.php';
 
 global $cri_container;// use global before using this
@@ -25,5 +26,10 @@ $cri_container = new Container();
 // Save instance of query builder
 $cri_container->set('query_builder', function(){
         return new QueryBuilder();
+    }
+);
+
+$cri_container->set('tools', function(){
+        return new CridonTools();
     }
 );
