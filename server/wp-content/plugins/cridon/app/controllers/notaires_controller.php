7a2796f2e10c285f0e35e1bd64759512d0c33de2
Joe <j.vanomaro@gmail.com>
2015-10-16 09:50:47 +0300

    Synchronisation données cri_notaire et cri_users

--- server/wp-content/plugins/cridon/app/controllers/notaires_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/notaires_controller.php
@@ -24,4 +24,31 @@ class NotairesController extends BasePublicController
 
 		parent::__construct();
 	}
+
+    /**
+     * Import Notaire into wp_users
+     */
+    public function import()
+    {
+        $rets = $this->model->importIntoWpUsers();
+
+        $this->set('rets', $rets);
+    }
+
+    /**
+     * Notaire dashbord
+     */
+    public function dashbord()
+    {
+        // check if user is not logged in
+        // or he's not a notaire
+        if ( !is_user_logged_in()
+             || ( !in_array( CONST_NOTAIRE_ROLE, $this->current_user->roles ) )
+        ) {
+            $adminUrl = get_bloginfo( 'url' ) . '/login/';
+            $this->redirect( $adminUrl );
+        }
+
+        $this->set('users', $this->current_user);
+    }
 }
--- server/wp-content/plugins/cridon/app/models/notaire.php
+++ server/wp-content/plugins/cridon/app/models/notaire.php
@@ -24,6 +24,11 @@ class Notaire extends MvcModel
     public $table = '{prefix}notaire';
 
     /**
+     * @var string
+     */
+    protected $logs;
+
+    /**
      * @var array
      */
     public $has_many = array(
@@ -41,5 +46,72 @@ class Notaire extends MvcModel
             'foreign_key' => 'ID'
         )
     );
+
+    /**
+     * @var mixed
+     */
+    protected $wpdb;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        global $wpdb;
+        $this->wpdb = $wpdb;
+
+        parent::__construct();
+    }
+
+    /**
+     * Action for importing notaire data into wp_users
+     */
+    public function importIntoWpUsers()
+    {
+        $this->importDataFromLocalTable();
+        return $this->logs;
+    }
+
+    /**
+     * Action for importing data from cri_notaire into wp_users
+     */
+    public function importDataFromLocalTable()
+    {
+        $this->logs = array();
+        $notaires   = $this->find();
+        foreach ($notaires as $notaire) {
+            // check if user already exist
+            $userName  = $notaire->crpcen;
+            $userId    = username_exists($userName);
+            $userDatas = array(
+                'user_login' => $userName,
+                'user_pass'  => $notaire->web_password,
+                'user_email' => $notaire->email_adress,
+                'first_name' => $notaire->first_name,
+                'last_name'  => $notaire->last_name,
+            );
+            if (!$userId) {
+                $userDatas['role'] = CONST_NOTAIRE_ROLE;
+
+                add_filter('sanitize_user', array($this, 'custom_sanitize_user'), 10, 3);
+                $userId = wp_insert_user($userDatas);
+                remove_filter('sanitize_user', array($this, 'custom_sanitize_user'), 10);
+
+                // On success
+                if (!is_wp_error($userId)) {
+                    array_push($this->logs, $userName . ' / userId : ' . $userId);
+
+                    // @TODO needed only if notaire data before wp_users
+                    // update notaire.id_wp_user
+                    $this->update($notaire->id, array('id_wp_user' => $userId));
+                }
+            } else {
+                $userDatas['ID'] = $userId;
+                $userDatas['user_nicename'] = sanitize_user($notaire->first_name . '_' . $notaire->last_name);
+                $userDatas['display_name']  = $notaire->first_name . ' ' . $notaire->last_name;
+                wp_update_user($userDatas);
+            }
+        }
+    }
 
 }
