ec7716250f8e1fddf8489936bf527b9e7fcaf75f
fmila <f.mila@etechconsulting-mg.com>
2015-10-20 09:07:02 +0300

    Chargement des classes et injections de dépendance.

--- server/wp-content/plugins/cridon/app/utils/class_loader.php
+++ server/wp-content/plugins/cridon/app/utils/class_loader.php
@@ -16,12 +16,15 @@ $pluginRoot = dirname( $dir );
 require_once 'Container.php';
 require_once $pluginRoot.'/models/query_builder.php';
 // All Posts Class
+require_once 'post/CridonPostStorage.php';
 require_once 'post/CridonPostUrl.php';
+require_once 'post/CridonPostStructure.php';
 require_once 'post/CridonPostParser.php';
 require_once 'post/CridonPostQuery.php';
 require_once 'post/CridonPostFactory.php';
 //
 require_once 'CridonTools.php';
+// Custom function used in template
 require_once 'functions.php';
 
 global $cri_container;// use global before using this
@@ -35,13 +38,18 @@ $cri_container->set('query_builder', function(){
     }
 );
 
-$cri_container->set('post_factory', function(){
-        return new CridonPostFactory();
+$cri_container->set('post_factory', function() use( $cri_container ){
+        return new CridonPostFactory( $cri_container->get( 'post_structure' ) );
+    }
+);
+
+$cri_container->set('post_structure', function(){
+        return new CridonPostStructure();
     }
 );
 
 $cri_container->set('tools', function() use( $cri_container ){
-        return new CridonTools( $cri_container->get( 'post_factory' ) );
+        return new CridonTools( $cri_container->get( 'post_factory' ),$cri_container->get( 'post_structure' ) );
     }
 );
 
