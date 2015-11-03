7ad7df2343a2ecdff0376f91e9dc89376b0dfe71
fmila <f.mila@etechconsulting-mg.com>
2015-11-03 12:38:35 +0300

    Liaison avec le model User.

--- server/wp-content/plugins/cridon/app/controllers/admin/admin_user_cridons_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/admin/admin_user_cridons_controller.php
@@ -2,8 +2,23 @@
 
 class AdminUserCridonsController extends MvcAdminController {
     
-    var $default_columns = array('id', 'name');
-    
+    var $default_columns = array('id_erp', 'profil',
+        'user' => array( 'label'=>'Utilisateur','value_method' => 'show_user'),
+        'last_connection' => array( 'value_method' => 'show_last_connection'),
+    );
+    public function show_user($object)
+    {
+        return empty($object->user) ? null : $object->user->__name;
+    }
+    public function show_last_connection($object)
+    {
+        $date = '';
+        if( !empty( $object->last_connection ) ){
+            $dt = new DateTime($object->last_connection);    
+            $date = $dt->format('d-m-Y H:i');//To FR
+        }
+        return $date;
+    }
 }
 
 ?>
--- server/wp-content/plugins/cridon/app/models/user_cridon.php
+++ server/wp-content/plugins/cridon/app/models/user_cridon.php
@@ -1,12 +1,13 @@
 <?php
 
 class UserCridon extends MvcModel {
-
+    var $table        = '{prefix}user_cridon';
     var $display_field = 'id_erp';
+    var $includes  = array('User');
+
     public $belongs_to = array(
         'User' => array(
-            'class'       => 'MvcUser',
-            'foreign_key' => 'ID'
+            'foreign_key' => 'id_wp_user'
         )
     );
     
