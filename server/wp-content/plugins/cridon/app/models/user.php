e2b2a204a078e990b553f42e0c7d338aba2bc112
fmila <f.mila@etechconsulting-mg.com>
2015-11-05 12:43:12 +0300

    Utilisation de MvcUser.

--- server/wp-content/plugins/cridon/app/models/user.php
+++ server/wp-content/plugins/cridon/app/models/user.php
@@ -1,9 +1,6 @@
 <?php
 
-class User extends MvcModel {
-    var $primary_key   = 'ID';
-    var $display_field = 'user_nicename';
-    var $table         = '{prefix}users';
+class User extends MvcUser {
     var $has_many      = array(
         'UserCridon' => array(
             'foreign_key' => 'id_wp_user'
