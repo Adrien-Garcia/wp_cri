ea82d6089deac0c55a955a7d2577c1ea21a42683
fmila <f.mila@etechconsulting-mg.com>
2015-11-12 13:37:28 +0300

    Correction liaison n-n entre competence et question.

--- server/wp-content/plugins/cridon/app/models/question.php
+++ server/wp-content/plugins/cridon/app/models/question.php
@@ -4,7 +4,7 @@ class Question extends MvcModel {
 
     var $display_field = 'srenum';
     var $table         = '{prefix}question';
-    var $includes      = array('Competence','Affectation','Support', 'Notaire');
+    var $includes      = array('Competence','Competences','Affectation','Support', 'Notaire');
     var $belongs_to    = array(
         'Competence' => array('foreign_key' => 'id_competence_1'),
         //'Competence_2' => array('foreign_key' => 'id_competence_2'),
@@ -12,7 +12,7 @@ class Question extends MvcModel {
         'Support' => array('foreign_key' => 'id_support')
     );
     var $has_and_belongs_to_many = array(
-        'Competence' => array(
+        'Competences' => array(
             'foreign_key' => 'id_question',
             'association_foreign_key' => 'id_competence',
             'join_table' => '{prefix}question_competence',
