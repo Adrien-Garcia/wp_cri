7e21c34e8a57c9e9064eaef136f3686b483ef368
fmila <f.mila@etechconsulting-mg.com>
2015-12-07 17:46:54 +0300

    Traduction des champs sur le formulaire des compétences.

--- server/wp-content/plugins/cridon/app/views/admin/competences/edit.php
+++ server/wp-content/plugins/cridon/app/views/admin/competences/edit.php
@@ -1,7 +1,7 @@
 <h2><?php echo Config::$titleAdminForm['competence']['edit'] ?></h2>
 
 <?php echo $this->form->create($model->name); ?>
-<?php echo $this->form->input('label'); ?>
-<?php echo $this->form->belongs_to_dropdown('Matiere', $aMatiere, array('style' => 'width: 200px;', 'empty' => false)); ?>
-<?php echo $this->form->checkbox_input('displayed', array('label' => 'Displayed')); ?>
+<?php echo $this->form->input('label',array('label' => Config::$titleFieldAdminForm['label'])); ?>
+<?php echo $this->form->belongs_to_dropdown('Matiere', $aMatiere, array('label' => 'Matière','style' => 'width: 200px;', 'empty' => false)); ?>
+<?php echo $this->form->checkbox_input('displayed', array('label' => Config::$titleFieldAdminForm['displayed'])); ?>
 <?php echo $this->form->end(Config::$btnTextAdmin['update']); ?>
