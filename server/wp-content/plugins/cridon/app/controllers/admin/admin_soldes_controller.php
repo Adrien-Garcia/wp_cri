3b68489c82f72281490797cc1e19b0a04904f155
fmila <f.mila@etechconsulting-mg.com>
2015-12-07 16:23:57 +0300

    Traduction sur le listing des soldes. Faire fonctionner le formulaire d'édition d'un solde.

--- server/wp-content/plugins/cridon/app/controllers/admin/admin_soldes_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/admin/admin_soldes_controller.php
@@ -7,5 +7,14 @@ class AdminSoldesController extends MvcAdminController
      * @var array
      */
     public $default_columns = array('id', 'client_number', 'quota', 'points');
-
+    
+    public function index() {
+        $this->init_default_columns();
+        $this->process_params_for_search();
+        $collection = $this->model->paginate($this->params);
+        $this->set('objects', $collection['objects']);
+        $this->set_pagination($collection);
+        //Load custom helper
+        $this->load_helper('AdminCustom');
+    }
 }
--- server/wp-content/plugins/cridon/app/views/admin/soldes/edit.php
+++ server/wp-content/plugins/cridon/app/views/admin/soldes/edit.php
@@ -1,5 +1,10 @@
-<h2>Edit Solde</h2>
+<h2>Edition d'un solde</h2>
 
 <?php echo $this->form->create($model->name); ?>
-<?php echo $this->form->input('name'); ?>
+<?php echo $this->form->input('client_number'); ?>
+<?php echo $this->form->input('quota'); ?>
+<?php echo $this->form->input('type_support'); ?>
+<?php echo $this->form->input('nombre'); ?>
+<?php echo $this->form->input('points'); ?>
+<?php echo $this->form->input('date_arret'); ?>
 <?php echo $this->form->end('Update'); ?>
