b41cb3f806d32437bd522806b8a9666c11c84261
fmila <f.mila@etechconsulting-mg.com>
2016-01-13 16:55:56 +0300

    Ajout de la route pour show/[id]

--- server/wp-content/plugins/cridon/app/config/routes.php
+++ server/wp-content/plugins/cridon/app/config/routes.php
@@ -36,7 +36,7 @@ MvcRouter::public_connect('notaires/{:id:[\d]+}', array('controller' => 'notaire
 // default
 MvcRouter::public_connect('{:controller}', array('action' => 'index'));
 MvcRouter::public_connect('{:controller}/{:action}/{:id:[\d]+}');
-
+MvcRouter::public_connect('{:controller}/{:id:[a-zA-Z0-9=+~_-]+}', array('action' => 'show'));
 
 //Ajax admin
 MvcRouter::admin_ajax_connect(array('controller' => 'admin_documents', 'action' => 'search'));
