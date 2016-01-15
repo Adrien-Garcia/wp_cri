f2ccea2cacdf84261df65c00d208d6870dbac176
fmila <f.mila@etechconsulting-mg.com>
2016-01-15 16:33:25 +0300

    Rendre à la vue listing, la matière séléctionner, s'il est unique on retourne l'objet sinon null.

--- server/wp-content/plugins/cridon/app/controllers/veilles_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/veilles_controller.php
@@ -13,6 +13,8 @@
  */
 
 class VeillesController extends MvcPublicController {
+    //unique matiere selected, default null
+    protected static $currentMatiereSelected = null;
     
     public function index() {
         $this->params['per_page'] = !empty($this->params['per_page']) ? $this->params['per_page'] : DEFAULT_POST_PER_PAGE;
@@ -27,25 +29,35 @@ class VeillesController extends MvcPublicController {
         //Order by date publish
         $this->params['order'] = 'Post.post_date DESC' ;
         $collection = $this->model->paginate($this->params);
-        
-        $this->set('objects', $collection['objects']);
-        $this->set_pagination($collection);
-        add_action('wp_head', array($this, 'addMetaHeader') );//hook 
-    }
-
-    public  function addMetaHeader() {
-        $meta_title = $meta_description = '';
-        $canonical = mvc_public_url(array('controller' => MvcInflector::tableize($this->model->name)));
+        $matiere = null;
         if( isset($_GET['matiere']) && !empty($_GET['matiere']) && is_array($_GET['matiere']) ){
+            //unique matiere
             if(count($_GET['matiere']) === 1){
                 $matiere = mvc_model('matiere')->find_one_by_virtual_name(esc_sql(strip_tags($_GET['matiere'][0])));
                 if($matiere){
-                    $meta_title = $matiere->meta_title;
-                    $meta_description = $matiere->meta_description;
-                    $canonical = mvc_public_url(array('controller' => MvcInflector::tableize($matiere->__model_name),'id' => $matiere->virtual_name));
+                    self::$currentMatiereSelected = $matiere;
                 }
             }
         }
+        //selected matiere
+        $this->set('matiere', $matiere);
+        $this->set('objects', $collection['objects']);
+        $this->set_pagination($collection);
+        add_action('wp_head', array($this, 'addMetaHeader') );//hook WP to append in header
+    }
+
+    public  function addMetaHeader() {
+        $meta_title = $meta_description = '';//initialize
+        $canonical = mvc_public_url(array('controller' => MvcInflector::tableize($this->model->name)));
+        //unique matiere
+        if( self::$currentMatiereSelected !== null ){
+            $matiere = self::$currentMatiereSelected;
+            $meta_title = $matiere->meta_title;
+            $meta_description = $matiere->meta_description;
+            //generate url
+            $canonical = mvc_public_url(array('controller' => MvcInflector::tableize($matiere->__model_name),'id' => $matiere->virtual_name));            
+        }
+        //variable to output in view
         $options = array(
             'locals' => array(
                 'meta_title'        => $meta_title,
@@ -53,6 +65,7 @@ class VeillesController extends MvcPublicController {
                 'canonical'         => $canonical
             )
         );
+        //render view meta
         $this->render_view_with_view_vars('veilles/meta', $options);
     }
     public function show() {
