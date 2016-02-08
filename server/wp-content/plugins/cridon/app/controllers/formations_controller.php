415e21f8744ad1453aea77646d94b8f52af60124
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-02-08 15:17:03 +0100

    Refactor fonction setObjects : passage par un controller intermédiaire

--- server/wp-content/plugins/cridon/app/controllers/formations_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/formations_controller.php
@@ -3,7 +3,8 @@
 /**
  * Class FormationsController
  */
-class FormationsController extends MvcPublicController
+require_once 'base_actu_controller.php';
+class FormationsController extends BaseActuController
 {
 
     /**
@@ -31,52 +32,5 @@ class FormationsController extends MvcPublicController
         $this->set('formationsFutures', $formationsFutures);
         $this->set('formationsPassees', $formationsPassees);
         $this->set_pagination($collection);
-    }
-
-    /**
-     * @override
-     * @return boolean
-     */
-    public function set_object() {
-        if (!empty($this->model->invalid_data)) {
-            if (!empty($this->params['id']) && empty($this->model->invalid_data[$this->model->primary_key])) {
-                $this->model->invalid_data[$this->model->primary_key] = $this->params['id'];
-            }
-            $object = $this->model->new_object($this->model->invalid_data);
-        } else if (!empty($this->params['id'])) {
-            $aObject = $this->model->find(
-                array(
-                    'joins' => array('Post'),
-                    'conditions' => array(
-                        'Post.post_name' => $this->params['id']
-                    )
-                )
-            );
-            if(!empty($aObject)){
-                $object = reset($aObject);
-            } else if (is_numeric($this->params['id'])) {
-                //if the url is numeric and if it's not a post name
-                $object = $this->model->find_by_id($this->params['id']);
-                if (!empty($object)) {
-                    $options = array(
-                        'controller' => $this->name,
-                        'action' => 'show',
-                        'id' => $object->post->post_name
-                    );
-                    $url = MvcRouter::public_url($options);
-                    //redirect to url with virtual-name
-                    wp_redirect($url, 301);
-                    exit;
-                }
-            }else{
-                $object = null;
-            }
-        }
-        if (!empty($object)) {
-            $this->set('object', $object);
-            MvcObjectRegistry::add_object($this->model->name, $this->object);
-            return true;
-        }
-        redirectTo404();
     }
 }
--- server/wp-content/plugins/cridon/app/controllers/vie_cridons_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/vie_cridons_controller.php
@@ -11,8 +11,8 @@
  * @contributor Fabrice MILA
  *
  */
-
-class VieCridonsController extends MvcPublicController {
+require_once 'base_actu_controller.php';
+class VieCridonsController extends BaseActuController {
     /*
      * We use the standard function for wordpress for queries ( query_posts() ) in views
      */
@@ -36,53 +36,6 @@ class VieCridonsController extends MvcPublicController {
         $this->set('objects', $collection['objects']);
         $this->set_pagination($collection);
 
-    }
-    
-    /**
-     * @override
-     * @return boolean
-     */
-    public function set_object() {
-        if (!empty($this->model->invalid_data)) {
-            if (!empty($this->params['id']) && empty($this->model->invalid_data[$this->model->primary_key])) {
-                $this->model->invalid_data[$this->model->primary_key] = $this->params['id'];
-            }
-            $object = $this->model->new_object($this->model->invalid_data);
-        } else if (!empty($this->params['id'])) {
-            $aObject = $this->model->find(
-               array(
-                   'joins' => array('Post'),
-                   'conditions' => array(
-                       'Post.post_name' => $this->params['id']
-                   )
-               )
-            );
-            if(!empty($aObject)){
-                $object = reset($aObject);
-            } else if (is_numeric($this->params['id'])) {
-                //if the url is numeric and if it's not a post name
-                $object = $this->model->find_by_id($this->params['id']);
-                if (!empty($object)) {
-                    $options = array(
-                        'controller' => $this->name,
-                        'action' => 'show',
-                        'id' => $object->post->post_name
-                    );
-                    $url = MvcRouter::public_url($options);
-                    //redirect to url with virtual-name
-                    wp_redirect($url, 301);
-                    exit;
-                }
-            }else{
-                $object = null;
-            }
-        }
-        if (!empty($object)) {
-            $this->set('object', $object);
-            MvcObjectRegistry::add_object($this->model->name, $this->object);
-            return true;
-        }
-        redirectTo404();
     }
 }
 
