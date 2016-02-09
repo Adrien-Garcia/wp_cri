bc97bb88f8a8aa4097476b646889ba2ade15d45d
Joe <j.vanomaro@gmail.com>
2016-02-09 15:13:12 +0300

    Suppression "/{id}/" sur les url du menu "Espace Privé" en Front

--- server/wp-content/themes/maestro/page-mon-compte.php
+++ server/wp-content/themes/maestro/page-mon-compte.php
@@ -18,8 +18,8 @@
  
 				<h1>Mon compte</h1>
 				<ul id="sel-compte">
-					<li class="js-account-dashboard js-account-blocs <?php echo (!isset($onglet) || $onglet == 1) ? " active " : ""?>" data-js-name="Dashboard" data-js-ajax-src="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/contentdashboard">
-						<a href="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/" class="bt js-account-dashboard-button">Tableaux de bord</a>
+					<li class="js-account-dashboard js-account-blocs <?php echo (!isset($onglet) || $onglet == 1) ? " active " : ""?>" data-js-name="Dashboard" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentdashboard">
+						<a href="<?php get_home_url() ?>/notaires/" class="bt js-account-dashboard-button">Tableaux de bord</a>
 						<div id="tableau-de-bord" class="pannel js-account-ajax">
                             <?php if (!isset($onglet) || $onglet == 1) : ?>
                                 <?php CriRenderView('contentdashboard', array(), 'notaires') ?>
@@ -27,16 +27,16 @@
 						</div>
 
 					</li>
-					<li class="js-account-questions js-account-blocs <?php echo ($onglet == 2) ? " active " : ""?>" data-js-name="Questions" data-js-ajax-src="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/contentquestions">
-						<a href="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/questions" class="bt js-account-questions-button">Mes Questions</a>
+					<li class="js-account-questions js-account-blocs <?php echo ($onglet == 2) ? " active " : ""?>" data-js-name="Questions" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentquestions">
+						<a href="<?php get_home_url() ?>/notaires/questions" class="bt js-account-questions-button">Mes Questions</a>
 						<div id="mes-questions" class="pannel js-account-ajax">
                             <?php if ($onglet == 2) : ?>
                                 <?php CriRenderView('contentquestions', array(), 'notaires') ?>
                             <?php endif; ?>
 						</div>
 					</li>
-					<li class="js-account-profil js-account-blocs <?php echo ($onglet == 3) ? " active " : ""?>" data-js-name="Profil" data-js-ajax-src="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/contentprofil">
-						<a href="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/profil" class="bt js-account-profil-button" id="sel-compte-profil-button">Mon profil</a>
+					<li class="js-account-profil js-account-blocs <?php echo ($onglet == 3) ? " active " : ""?>" data-js-name="Profil" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentprofil">
+						<a href="<?php get_home_url() ?>/notaires/profil" class="bt js-account-profil-button" id="sel-compte-profil-button">Mon profil</a>
 						<div id="mon-profil" class="pannel js-account-ajax">
                             <?php if ($onglet == 3) : ?>
                                 <?php CriRenderView('contentprofil', array(), 'notaires') ?>
@@ -44,8 +44,8 @@
 						</div>
 					</li>
 					<?php if (CriCanAccessFinance()): ?>
-					<li class="js-account-facturation js-account-blocs <?php echo ($onglet == 4) ? " active " : ""?>" data-js-name="Facturation" data-js-ajax-src="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/contentfacturation">
-						<a href="<?php get_home_url() ?>/notaires/<?php echo $id ; ?>/facturation" class="bt js-account-facturation-button">Règles de facturation</a>
+					<li class="js-account-facturation js-account-blocs <?php echo ($onglet == 4) ? " active " : ""?>" data-js-name="Facturation" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentfacturation">
+						<a href="<?php get_home_url() ?>/notaires/facturation" class="bt js-account-facturation-button">Règles de facturation</a>
 						<div id="regles-facturation" class="pannel js-account-ajax">
                         <?php if ($onglet == 4) : ?>
                         	<?php CriRenderView('contentprofil', array(), 'facturation') ?>
