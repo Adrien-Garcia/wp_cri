b9f21bef473d5d2819918c6e8c9d14d63b63d736
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-02-16 10:46:00 +0100

    Ajout des champs code postal et ville dans la table des formations

--- server/wp-content/plugins/cridon/migrations/047_alter_formations_adresse.sql
+++ server/wp-content/plugins/cridon/migrations/047_alter_formations_adresse.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `cri_formation` ADD `address` VARCHAR(100) NOT NULL;
+ALTER TABLE `cri_formation` ADD `address` VARCHAR(50) NOT NULL;
+ALTER TABLE `cri_formation` ADD `postal_code` VARCHAR(10) NOT NULL;
+ALTER TABLE `cri_formation` ADD `town` VARCHAR(40) NOT NULL;
