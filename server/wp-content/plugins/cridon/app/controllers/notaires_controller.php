20aaf40d1c073d0429855fad26e1381df2bb6ca4
Joe <j.vanomaro@gmail.com>
2016-02-22 17:36:08 +0300

    Suppression methode inutile

--- server/wp-content/plugins/cridon/app/controllers/notaires_controller.php
+++ server/wp-content/plugins/cridon/app/controllers/notaires_controller.php
@@ -26,21 +26,6 @@ class NotairesController extends BasePublicController
     }
 
     /**
-     * Generate error
-     *
-     * @global \WP_Query $wp_query
-     */
-    private function generateError(){
-        global $wp_query;
-        header("HTTP/1.0 404 Not Found - Archive Empty");
-        $wp_query->set_404();
-        if( file_exists(TEMPLATEPATH.'/404.php') ){
-            require TEMPLATEPATH.'/404.php';
-        }
-        exit;
-    }
-
-    /**
      * Secure Access Page
      *
      * @return void
