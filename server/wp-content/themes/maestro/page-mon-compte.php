796dd43b5a087013f077f408e44c8b588b6099f6
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-03-15 17:10:07 +0100

    fix - Déplacement des classes analytics des <li> vers les <a>

--- server/wp-content/themes/maestro/page-mon-compte.php
+++ server/wp-content/themes/maestro/page-mon-compte.php
@@ -14,8 +14,8 @@
  
 				<h1>Mon compte</h1>
 				<ul id="sel-compte">
-					<li class="js-account-dashboard js-account-blocs analytics_Dashboard_dashboard <?php echo (!isset($onglet) || $onglet == 1) ? " active " : ""?>" data-js-name="Dashboard" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentdashboard">
-						<a href="<?php get_home_url() ?>/notaires/" class="bt js-account-dashboard-button">Tableaux de bord</a>
+					<li class="js-account-dashboard js-account-blocs <?php echo (!isset($onglet) || $onglet == 1) ? " active " : ""?>" data-js-name="Dashboard" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentdashboard">
+						<a href="<?php get_home_url() ?>/notaires/" class="bt js-account-dashboard-button analytics_Dashboard_dashboard">Tableaux de bord</a>
 						<div id="tableau-de-bord" class="pannel js-account-ajax">
                             <?php if (!isset($onglet) || $onglet == 1) : ?>
                                 <?php CriRenderView('contentdashboard', array('controller' => $this, 'questions' => $questions, 'notaire' => $notaire), 'notaires') ?>
@@ -23,16 +23,16 @@
 						</div>
 
 					</li>
-					<li class="js-account-questions js-account-blocs analytics_Dashboard_questions <?php echo ($onglet == 2) ? " active " : ""?>" data-js-name="Questions" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentquestions">
-						<a href="<?php get_home_url() ?>/notaires/questions" class="bt js-account-questions-button">Mes Questions</a>
+					<li class="js-account-questions js-account-blocs <?php echo ($onglet == 2) ? " active " : ""?>" data-js-name="Questions" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentquestions">
+						<a href="<?php get_home_url() ?>/notaires/questions" class="bt js-account-questions-button analytics_Dashboard_questions">Mes Questions</a>
 						<div id="mes-questions" class="pannel js-account-ajax">
                             <?php if ($onglet == 2) : ?>
                                 <?php CriRenderView('contentquestions', array('notaire' => $notaire, 'answered' => $answered,'pending'=> $pending,'juristesPending'=> $juristesPending,'juristesAnswered' => $juristesAnswered,'matieres' => $matieres,'controller' => $this), 'notaires') ?>
                             <?php endif; ?>
 						</div>
 					</li>
-					<li class="js-account-profil js-account-blocs analytics_Dashboard_profil <?php echo ($onglet == 3) ? " active " : ""?>" data-js-name="Profil" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentprofil">
-						<a href="<?php get_home_url() ?>/notaires/profil" class="bt js-account-profil-button" id="sel-compte-profil-button">Mon profil</a>
+					<li class="js-account-profil js-account-blocs <?php echo ($onglet == 3) ? " active " : ""?>" data-js-name="Profil" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentprofil">
+						<a href="<?php get_home_url() ?>/notaires/profil" class="bt js-account-profil-button analytics_Dashboard_profil" id="sel-compte-profil-button">Mon profil</a>
 						<div id="mon-profil" class="pannel js-account-ajax">
                             <?php if ($onglet == 3) : ?>
                                 <?php CriRenderView('contentprofil', array('matieres' => $matieres,'notaire' => $notaire), 'notaires') ?>
@@ -40,8 +40,8 @@
 						</div>
 					</li>
 					<?php if (CriCanAccessFinance()): ?>
-					<li class="js-account-facturation js-account-blocs analytics_Dashboard_facturation <?php echo ($onglet == 4) ? " active " : ""?>" data-js-name="Facturation" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentfacturation">
-						<a href="<?php get_home_url() ?>/notaires/facturation" class="bt js-account-facturation-button">Règles de facturation</a>
+					<li class="js-account-facturation js-account-blocs <?php echo ($onglet == 4) ? " active " : ""?>" data-js-name="Facturation" data-js-ajax-src="<?php get_home_url() ?>/notaires/contentfacturation">
+						<a href="<?php get_home_url() ?>/notaires/facturation" class="bt js-account-facturation-button analytics_Dashboard_facturation">Règles de facturation</a>
 						<div id="regles-facturation" class="pannel js-account-ajax">
                         <?php if ($onglet == 4) : ?>
                         	<?php CriRenderView('contentprofil', array('notaire' => $notaire, 'content' => $content), 'facturation') ?>
