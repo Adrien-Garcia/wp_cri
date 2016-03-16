6cc73debd0ff305ff17b10e4e9df778128a15b08
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-03-16 16:57:58 +0100

    Cablage onglet 'collaborateur'

--- server/wp-content/themes/maestro/library/js/application/account.js
+++ server/wp-content/themes/maestro/library/js/application/account.js
@@ -9,6 +9,7 @@ App.Account = {
     accountQuestionSelector             : '-questions',
     accountProfilSelector               : '-profil',
     accountFacturationSelector          : '-facturation',
+    accountCollaborateurSelector        : '-collaborateur',
 
     accountQuestionMoreSelector         : '-more',
     accountProfilSubscriptionSelector   : '-subscription',
@@ -41,15 +42,18 @@ App.Account = {
     $accountQuestion                    : null,
     $accountProfil                      : null,
     $accountFacturation                 : null,
+    $accountCollaborateur               : null,
     $accountDashboardAjax               : null,
     $accountQuestionAjax                : null,
     $accountProfilAjax                  : null,
     $accountFacturationAjax             : null,
+    $accountCollaborateurAjax           : null,
 
     $accountDashboardButton             : null,
     $accountQuestionButton              : null,
     $accountProfilButton                : null,
     $accountFacturationButton           : null,
+    $accountCollaborateurButton         : null,
 
     $accountQuestionMoreButton          : null,
 
@@ -85,20 +89,23 @@ App.Account = {
 
         this.$accountBlocks             = $(d + this.accountBlocksSelector);
 
-        this.$accountDashboardButton    = $(d + this.accountDashboardSelector + b);
-        this.$accountQuestionButton     = $(d + this.accountQuestionSelector + b);
-        this.$accountProfilButton       = $(d + this.accountProfilSelector + b);
-        this.$accountFacturationButton  = $(d + this.accountFacturationSelector + b);
+        this.$accountDashboardButton     = $(d + this.accountDashboardSelector + b);
+        this.$accountQuestionButton      = $(d + this.accountQuestionSelector + b);
+        this.$accountProfilButton        = $(d + this.accountProfilSelector + b);
+        this.$accountFacturationButton   = $(d + this.accountFacturationSelector + b);
+        this.$accountCollaborateurButton = $(d + this.accountCollaborateurSelector + b);
 
-        this.$accountDashboard          = $(d + this.accountDashboardSelector);
-        this.$accountQuestion           = $(d + this.accountQuestionSelector);
-        this.$accountProfil             = $(d + this.accountProfilSelector);
-        this.$accountFacturation        = $(d + this.accountFacturationSelector);
+        this.$accountDashboard           = $(d + this.accountDashboardSelector);
+        this.$accountQuestion            = $(d + this.accountQuestionSelector);
+        this.$accountProfil              = $(d + this.accountProfilSelector);
+        this.$accountFacturation         = $(d + this.accountFacturationSelector);
+        this.$accountCollaborateur       = $(d + this.accountCollaborateurSelector);
 
-        this.$accountDashboardAjax      = this.$accountDashboard.find(d + a);
-        this.$accountQuestionAjax       = this.$accountQuestion.find(d + a);
-        this.$accountProfilAjax         = this.$accountProfil.find(d + a);
-        this.$accountFacturationAjax    = this.$accountFacturation.find(d + a);
+        this.$accountDashboardAjax       = this.$accountDashboard.find(d + a);
+        this.$accountQuestionAjax        = this.$accountQuestion.find(d + a);
+        this.$accountProfilAjax          = this.$accountProfil.find(d + a);
+        this.$accountFacturationAjax     = this.$accountFacturation.find(d + a);
+        this.$accountCollaborateurAjax   = this.$accountCollaborateur.find(d + a);
 
         this.$accountBlocks.each(function(i, e) {
             if ($(e).hasClass('active')) {
@@ -187,6 +194,12 @@ App.Account = {
 
     },
 
+    initCollaborateur: function() {
+        this.debug('Account : Init Collaborateur');
+        this.addListenersCollaborateur();
+
+    },
+
     /*
      * Listeners for the Account page events
      */
@@ -215,6 +228,11 @@ App.Account = {
             e.returnValue = false;
             e.preventDefault();
             self.eventAccountFacturationOpen($(this));
+        });
+        this.$accountCollaborateurButton.on("click", function(e) {
+            e.returnValue = false;
+            e.preventDefault();
+            self.eventAccountCollaborateurOpen($(this));
         });
 
         this.debug("Account : addListeners end");
@@ -287,6 +305,18 @@ App.Account = {
         var self = this;
 
         this.debug("Account : addListenersFacturation");
+
+
+    },
+
+    /*
+     * Listeners for the Account Collaborateur
+     */
+
+    addListenersCollaborateur: function() {
+        var self = this;
+
+        this.debug("Account : addListenersCollaborateur");
 
 
     },
@@ -365,6 +395,26 @@ App.Account = {
                 self.$accountFacturationAjax.html(data);
                 self.debug('Account Facturation Loaded');
                 self.initFacturation();
+            }
+        });
+        App.Utils.scrollTop();
+
+    },
+
+    /*
+     * Event for Opening the Collaborateur (AJAX)
+     */
+    eventAccountCollaborateurOpen: function() {
+        var self = this;
+        this.$accountBlocks.removeClass("active");
+        this.$accountCollaborateur.addClass("active");
+        $.ajax({
+            url: this.$accountCollaborateur.data('js-ajax-src'),
+            success: function(data)
+            {
+                self.$accountCollaborateurAjax.html(data);
+                self.debug('Account Collaborateur Loaded');
+                self.initCollaborateur();
             }
         });
         App.Utils.scrollTop();
