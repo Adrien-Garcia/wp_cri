1609ba1ed0687a5a63aaaf688cd3724e3547a71a
Alexandra Petit <alexandra.petit@jetpulp.fr>
2016-04-06 10:33:24 +0200

    style popup cridonline

--- server/wp-content/themes/maestro/content-cridonline-validation-popup.php
+++ server/wp-content/themes/maestro/content-cridonline-validation-popup.php
@@ -1,20 +1,27 @@
 <?php if (CriIsNotaire()) : ?>
 <div id="layer-cridonline" style="display:none;">
+
+    <div class="titre">
+        <span class="close_layer layer-cridonline_close"></span>
+        <span class="texte">
+            <?php _e('Souscription'); ?>
+            <span><?php _e('à l\'offre crid\'online'); ?></span>
+        </span>
+    </div>
+
     <div class="block js-home-block-link js-account-cridonline-validation-step1">
         <div class="content">
-            <div class="h2">
-                <span><?php _e('Veuillez imprimer, remplir et nous envoyer ce formulaire pour mise en place du prélèvement :'); ?></span>
-            </div>
+            <p><?php _e('Veuillez imprimer, remplir et nous envoyer ce formulaire pour mise en place du prélèvement :'); ?></p>
             <a href="/formulaire-prelevement.pdf" download class="js-account-cridonline-validation-toggle">Télécharger le formulaire</a>
         </div>
     </div>
     <div class="block js-home-block-link js-account-cridonline-validation-step2" style="display:none;">
         <div class="content">
-            <div class="h2">
-                <span><?php _e('Merci pour votre souscription, le service est désormais activé.'); ?></span>
-                <span><?php _e('Un email de confirmation vous a été envoyé .'); ?></span>
-            </div>
-            <a href="/veilles">J'ai compris : découvrir l'offre de veille</a>
+            <p>
+                <?php _e('Merci pour votre souscription, le service est désormais activé.'); ?><br />
+                <?php _e('Un email de confirmation vous a été envoyé.'); ?>
+            </p>            
+            <a href="/veilles"><!-- J'ai compris : découvrir l'offre de veille --> Découvrir l'offre CRID'ONLINE</a>
         </div>
     </div>
 </div>
