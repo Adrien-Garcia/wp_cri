514c7a15aeaffaa210605db85c7866d19a6fe922
Victor ALBERT <victor.albert@jetpulp.fr>
2016-04-13 12:45:42 +0200

    acces Crid'Online : redirection après connexion en place

--- server/wp-content/themes/maestro/library/js/application/cridonline.js
+++ server/wp-content/themes/maestro/library/js/application/cridonline.js
@@ -6,16 +6,24 @@ App.Cridonline = {
     linkMark    : null,
     linkValue   : '',
     init: function() {
+        var current_location = window.location;
+
         this.debug("CridOnline : init start");
 
         this.linkMark = $('#' + this.prefix + this.linkSuffix);
 
-        if (typeof wkf !== 'undefined') {
-            this.linkValue = wkf.url;
-            this.linkMark.attr('href', this.linkValue);
+        if (typeof wkf === 'undefined') {
+            // Do not preserve current Query String : not needed as the purpose is to get to Crid'Online page
+            this.linkValue = window.location + '?openLogin=1&messageLogin=PROTECTED_CONTENT&requestUrl=';
+            this.linkValue += encodeURIComponent(current_location + '?openCridonOnline=1');
         } else {
-            this.linkMark.attr('href', '?openLogin=1&messageLogin=PROTECTED_CONTENT');
+            this.linkValue = wkf.url;
+            if (App.Utils.queryString['openCridonOnline'] == "1") {
+                // if coming from login panel, redirect to Crid'Online page
+                window.location = this.linkValue;
+            }
         }
+        this.linkMark.attr('href', this.linkValue);
 
         this.debug("CridOnline : init end");
     },
