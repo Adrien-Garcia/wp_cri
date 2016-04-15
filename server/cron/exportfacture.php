78c88770f775f477e01a3f53d041675ab40b81e4
Joe <j.vanomaro@gmail.com>
2016-04-15 16:47:40 +0300

    Fichier cron import facture

---
+++ server/cron/exportfacture.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Description of exportfacture.php
+ *
+ * @package wp_cridon
+ * @author eTech
+ * @contributor Joelio
+ */
+
+// load WP Core
+require_once '../wp-load.php';
+
+// etude model
+/**
+ * @var $model Etude
+ */
+$model = mvc_model('etude');
+// call export action
+$code = $model->exportFacture();
+
+echo $code;
