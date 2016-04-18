76a677cd9e1d042fbe752a04c4b54a2662bdb1f7
Joe <j.vanomaro@gmail.com>
2016-04-18 16:40:59 +0300

    Traitement d'import document

--- server/wp-content/plugins/cridon/app/models/etude.php
+++ server/wp-content/plugins/cridon/app/models/etude.php
@@ -31,8 +31,118 @@ class Etude extends \App\Override\Model\CridonMvcModel {
         }
     }
 
+    /**
+     * Import Releve action
+     *
+     * @throws Exception
+     */
+    public function importReleveconso()
+    {
+        // documents
+        $Directory  = new RecursiveDirectoryIterator(CONST_IMPORT_RELEVECONSO_TEMP_PATH);
+        $Iterator   = new RecursiveIteratorIterator($Directory);
+        $documents  = new RegexIterator($Iterator, '/^.+\.pdf$/i', RecursiveRegexIterator::GET_MATCH);
 
+        // offset block
+        $limit      = 1000;
+
+        // date
+        $date = date('Ym');
+
+        $documentModel = mvc_model('Document');
+
+        $this->importRelevePdf($documents, $Iterator, $limit, $date, $documentModel);
+    }
+
+    /**
+     * Import des fichiers de façon iteratif
+     *
+     * @param array $documents
+     * @param mixed $Iterator
+     * @param int $limit
+     * @param string $date
+     * @param mixed $documentModel
+     */
+    protected function importRelevePdf($documents, $Iterator, $limit, $date, $documentModel)
+    {
+        foreach(new LimitIterator($documents, 0, $limit + 1) as $document) {
+            try {
+                if (!empty($document[0])) { // document existe
+                    $fileInfo = pathinfo($document[0]);
+                    if (!empty($fileInfo['basename'])) {
+                        // filtre les fichiers selon la regle de nommage predefinie
+                        // ACs : <CRPCEN_releveconso_AAAAMMJJ>.pdf
+                        // @see \Config::$importRelevePattern
+                        if (preg_match_all(Config::$importRelevePattern, $fileInfo['basename'], $matches)) {
+                            $path = CONST_IMPORT_RELEVECONSO_PATH . $date . DIRECTORY_SEPARATOR;
+                            if (!file_exists($path)) { // repertoire manquant
+                                // creation du nouveau repertoire
+                                wp_mkdir_p($path);
+                            }
+                            // CRPCEN present
+                            if (!empty($matches[1][0]) && rename($document[0], $path . $fileInfo['basename'])) {
+                                $crpcen      = $matches[1][0];
+
+                                // nouveau document
+                                if (!$documentModel->isDocExiste($fileInfo['basename'])) {
+                                    // donnees document
+                                    $docData = array(
+                                        'Document' => array(
+                                            'file_path'     => '/releveconso/' . $date . '/' . $fileInfo['basename'],
+                                            'download_url'  => '/documents/download/' . $crpcen,
+                                            'date_modified' => date('Y-m-d H:i:s'),
+                                            'type'          => CONST_DOC_TYPE_RELEVECONSO,
+                                            'id_externe'    => $crpcen,
+                                            'name'          => $fileInfo['basename'],
+                                        )
+                                    );
+
+                                    // insertion données
+                                    $documentId = $documentModel->create($docData);
+
+                                    // maj download_url
+                                    if ($documentId) {
+                                        $docData = array(
+                                            'Document' => array(
+                                                'id'           => $documentId,
+                                                'download_url' => '/documents/download/' . $documentId
+                                            )
+                                        );
+                                        $documentModel->save($docData);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // liberation des variables
+                    unset($fileInfo);
+                    unset($document);
+                    unset($matches);
+                    unset($crpcen);
+                }
+            } catch(Exception $e) {
+                // renommage fichier d'erreur
+                rename($document[0],
+                       str_replace(
+                           array('.pdf', '.PDF', '.Pdf'),
+                           array('.pdf.error', '.PDF.error', '.Pdf.error'),
+                           $document[0]
+                       )
+                );
+
+                writeLog($e, 'importreleveconso.log');
+            }
+        }
+
+        $documents  = new RegexIterator($Iterator, '/^.+\.pdf$/i', RecursiveRegexIterator::GET_MATCH);
+        // test s'il y a encore de fichier
+        $documents->next();
+        $doc = $documents->current();
+        if (!empty($doc)) {
+            // appel action d'import
+            $documents->rewind();
+            $this->importRelevePdf($documents, $Iterator, $limit, $date, $documentModel);
+        }
+    }
 
 }
-
-?>
