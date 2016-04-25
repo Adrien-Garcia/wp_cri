27d01ebb9c49b60e0048344e7cc53ce485d21cce
Joe <j.vanomaro@gmail.com>
2016-04-25 12:56:56 +0300

    Paramettrage capabilités

--- server/wp-content/plugins/cridon/app/config/const.inc.php
+++ server/wp-content/plugins/cridon/app/config/const.inc.php
@@ -28,6 +28,12 @@ if ( !defined( 'CONST_QUESTIONECRITES_ROLE' ) ) {
 if ( !defined( 'CONST_QUESTIONTELEPHONIQUES_ROLE' ) ) {
     define( 'CONST_QUESTIONTELEPHONIQUES_ROLE', 'accesquesttelephoniques' );
 }
+if ( !defined( 'CONST_NOTAIRE_DIV_ROLE' ) ) {
+    define( 'CONST_NOTAIRE_DIV_ROLE', 'notaire_div' );
+}
+if ( !defined( 'CONST_NOTAIRE_ORG_ROLE' ) ) {
+    define( 'CONST_NOTAIRE_ORG_ROLE', 'notaire_org' );
+}
 // administrator role
 if ( !defined( 'CONST_ADMIN_ROLE' ) ) {
     define( 'CONST_ADMIN_ROLE', 'administrator' );
@@ -748,6 +754,21 @@ if ( !defined( 'CONST_ONGLET_CRIDONLINE' ) ) {
 if ( !defined( 'CONST_ONGLET_COLLABORATEUR' ) ) {
     define( 'CONST_ONGLET_COLLABORATEUR', 6 );
 }
+// capabilités pour les niveaux d'accès
+if ( !defined( 'CONST_ACCESS_LEVEL_1' ) ) {
+    define( 'CONST_ACCESS_LEVEL_1', 'access_level_1' );
+}
+if ( !defined( 'CONST_ACCESS_LEVEL_2' ) ) {
+    define( 'CONST_ACCESS_LEVEL_2', 'access_level_2' );
+}
+if ( !defined( 'CONST_ACCESS_LEVEL_3' ) ) {
+    define( 'CONST_ACCESS_LEVEL_3', 'access_level_3' );
+}
+// capabilités d'aceès au solde
+if ( !defined( 'CONST_ACCESS_SOLDE' ) ) {
+    define( 'CONST_ACCESS_SOLDE', 'access_solde' );
+}
+
 
 
 
