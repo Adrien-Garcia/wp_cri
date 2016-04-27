c5df9fe6731cc7aaa6d5bdf6958207b60024656c
Joe <j.vanomaro@gmail.com>
2016-04-27 14:45:58 +0300

    Amelioration traitement de purge des fichiers

--- server/cron/purgereleveconso.php
+++ server/cron/purgereleveconso.php
@@ -14,54 +14,53 @@ require_once '../wp-load.php';
  * Recuperation des documents à supprimer
  */
 global $wpdb;
+
+// recuperation upload path
+$uploadDir = wp_upload_dir();
+
+// preparation requette
 $sql = "
         SELECT
-                `d`.`id_externe`, `d`.`file_path`, `d`.`label` AS aaaammdd
+                `d`.`id_externe` crpcen, `d`.`file_path`, `d`.`label` aaaammdd
         FROM
                 cri_document d
         WHERE `type` = 'releveconso'
+        ORDER BY `id_externe`, `label` DESC
       ";
-$resp = $wpdb->get_results($sql);
+// recuperation resultats
+$conso = $wpdb->get_results($sql);
 
-$listDocs       = array();
-$listDocPath    = array();
-foreach ($resp as $item) {
+// traitement des resultats
+foreach ($conso as $key => $item) {
     if (!empty($item->aaaammdd)) {
         // recuperation de l'annee AAAA
         $year = substr($item->aaaammdd, 0, 4);
         // exclure l'annee en cours
         if ($year < date('Y')) {
-            // regroupement de tous les docs de meme annee par etude
-            $listDocs[$year][$item->id_externe][] = $item->aaaammdd;
-            // chemin absolu correspondant au doc (utile pour la suppression)
-            $listDocPath[$item->id_externe][$item->aaaammdd] = $item->file_path;
-        }
-    }
-}
+            if ($key == 0) { // premiere iteration
+                $crpcenTemp = $item->crpcen;
+                $yearTemp   = $year;
+                $dateTemp   = $item->aaaammdd;
+            } else { // prochaines iterations
+                if ($crpcenTemp == $item->crpcen) { // meme crpcen
+                    if ($yearTemp == $year && $item->aaaammdd < $dateTemp) { // document meme annee
+                        $file = $uploadDir['basedir'] . $item->file_path;
 
-/**
- * Traitement de purge des fichiers
- */
-$uploadDir = wp_upload_dir();
-foreach($listDocs as $year => $docByCRPCEN) {
-    foreach($docByCRPCEN as $crpcen => $docs) {
-        // tri par ordre croissant des elements
-        sort($docs);
+                        // suppression enregistrement de la BDD
+                        $wpdb->query(" DELETE FROM {$wpdb->prefix}document WHERE file_path = '{$item->file_path}' ");
 
-        // depile le dernier element (faut garder la derniere doc de recap)
-        array_pop($docs);
-        foreach($docs as $aaaammjj) {
-            if (!empty($listDocPath[$crpcen][$aaaammjj])) {
-                $filePath = $listDocPath[$crpcen][$aaaammjj];
-                $file = $uploadDir['basedir'] . $listDocPath[$crpcen][$aaaammjj];
-
-                // suppression enregistrement de la BDD
-                $wpdb->query(" DELETE FROM {$wpdb->prefix}document
-                               WHERE file_path = '{$filePath}'
-                ");
-
-                // supression physique du fichier correspondant
-                @unlink($file);
+                        // supression physique du fichier correspondant
+                        @unlink($file);
+                    } else { // on change d'annee
+                        $crpcenTemp = $item->crpcen;
+                        $yearTemp   = $year;
+                        $dateTemp   = $item->aaaammdd;
+                    }
+                } else { // on passe au prochain crpcen
+                    $crpcenTemp = $item->crpcen;
+                    $yearTemp   = $year;
+                    $dateTemp   = $item->aaaammdd;
+                }
             }
         }
     }
