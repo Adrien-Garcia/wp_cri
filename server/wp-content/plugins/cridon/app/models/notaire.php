d6d0023c4e809dbd7ee59e08ee5f2d6d31b97b55
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-05-13 15:33:28 +0200

    Ajout des roles en fonction des fonctions notaires

--- server/wp-content/plugins/cridon/app/models/notaire.php
+++ server/wp-content/plugins/cridon/app/models/notaire.php
@@ -2909,15 +2909,11 @@ class Notaire extends \App\Override\Model\CridonMvcModel
                         $user->add_role(CONST_NOTAIRE_ORG_ROLE);
                     }
                 }
-                /**
-                 * finance role
-                 * to be matched in list of authorized user by function
-                 *
-                 * @see \Config::$canAccessFinance
-                 */
-                if (in_array($notary->id_fonction, Config::$canAccessFinance)) {
-                    $user->add_role(CONST_FINANCE_ROLE);
-                    $user->add_role(CONST_COLLABORATEUR_TAB_ROLE);
+                $rolesNotaire = Config::$notaryRolesByFunction['notaries'];
+                if (!empty($rolesNotaire[$notary->id_fonction])){
+                    foreach ($rolesNotaire[$notary->id_fonction] as $role){
+                        $user->add_role($role);
+                    }
                 }
 
                 // disable admin bar
