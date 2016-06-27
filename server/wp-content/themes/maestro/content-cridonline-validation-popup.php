200352feedb58bf5f0f5ec22c67daf7035a68dec
Renaud Amsellem <renaud.jetpulp@gmail.com>
2016-06-27 12:25:21 +0200

    Deux phrases dans a popup de confirmation de sous cridonline uniquement pour personne morale

--- server/wp-content/themes/maestro/content-cridonline-validation-popup.php
+++ server/wp-content/themes/maestro/content-cridonline-validation-popup.php
@@ -10,8 +10,10 @@
     <div class="block js-home-block-link js-account-cridonline-validation-step1">
         <div class="content">
             <p><?php _e('Veuillez imprimer, remplir et nous envoyer ce formulaire accompagné d\'un RIB pour mise en place du prélèvement.'); ?></p>
-            <p><?php _e('Une copie de ce formulaire doit également être impérativement adressée à votre banque.'); ?></p>
-            <p><?php _e('Nous attirons votre attention sur le fait qu\'à défaut le prélèvement sera rejeté et des frais bancaires vous seront facturés.'); ?></p>
+            <?php if ( $B2B_B2C == 'B2B') : ?>
+                <p><?php _e('Une copie de ce formulaire doit également être impérativement adressée à votre banque.'); ?></p>
+                <p><?php _e('Nous attirons votre attention sur le fait qu\'à défaut le prélèvement sera rejeté et des frais bancaires vous seront facturés.'); ?></p>
+            <?php endif; ?>
             <a target="_blank"
                href="<?php
                 if ( $B2B_B2C == 'B2B') {
