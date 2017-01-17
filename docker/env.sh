196ada5dad13b1c9936560d842983d2e9e0c3e5e
Victor ALBERT <victor.albert@jetpulp.fr>
2017-01-17 14:05:27 +0100

    fix docker conf : sql replace is enough to fix DB, no need for init-db.sh

--- docker/env.sh
+++ docker/env.sh
@@ -18,6 +18,8 @@ function init {
     export MYSQL_DATABASE=wp_cridon
     # every vitural_host separated by , (used for nginx proxy)
     export VIRTUAL_HOST=$SERVER_NAME,images-$SERVER_NAME,static-$SERVER_NAME
+    # timeout for DB restoring waiting (default 3m0s)
+    export DB_RESTORE_TIMEOUT=5m0s
 
 }
 
