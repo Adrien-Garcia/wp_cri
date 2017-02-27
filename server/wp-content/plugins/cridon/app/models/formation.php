84ad6287d2566fc9a59348312d6aa7c70e28d47f
Clément Horgues <clement.horgues@jetpulp.fr>
2017-02-27 18:05:42 +0100

    FACTO mails

--- server/wp-content/plugins/cridon/app/models/formation.php
+++ server/wp-content/plugins/cridon/app/models/formation.php
@@ -18,26 +18,11 @@ class Formation extends \App\Override\Model\CridonMvcModel
     var $display_field = 'name';
 
     public function sendEmailPreinscription($session, $formationParticipants, $formationCommentaire) {
-        // en-tete email
-        $headers = array('Content-Type: text/html; charset=UTF-8');
-        $notaire = mvc_model('Notaire')->getUserConnectedData();
-
-        // list des notaires à notifier par etude
-        $notary = array($notaire);
-
-        $adminDest = Config::$notificationAddressFormulaireFormation;
-        $dest        = array();
-        if (is_array($notary) && count($notary) > 0) {
-            foreach ($notary as $item) {
-                if (filter_var($item->email_adress, FILTER_VALIDATE_EMAIL)) {
-                    $dest[] = $item->email_adress;
-                }
-            }
-        }
+        $data = $this->_prepareNotificationsMails();
 
         // destinataire non vide
-        if (count($dest) > 0) {
-            array_unique($dest);
+        if (count($data['dest']) > 0) {
+            array_unique($data['dest']);
             $vars    = array(
                 'type'                  => CONST_FORMATION_PREINSCRIPTION,
                 'date'                  => strftime('%d %b %Y',strtotime($session->date)),
@@ -45,100 +30,50 @@ class Formation extends \App\Override\Model\CridonMvcModel
                 'organisme'             => strtoupper($session->organisme->name. ' ' . $session->organisme->city),
                 'participants'          => $formationParticipants,
                 'commentaire'           => $formationCommentaire,
-                'notaire'               => array(
-                    'crpcen'                => $notaire->crpcen,
-                    'fname'                 => $notaire->first_name,
-                    'lname'                 => $notaire->last_name,
-                    'mail'                  => !empty($notaire->email_adress) ? $notaire->email_adress : '',
-                ),
+                'notaire'               => $data['notaire'],
             );
-            $message = CriRenderView('mail_notification_formation', $vars, 'custom', false);
-            $messageAdmin = CriRenderView('mail_notification_admin_formation', $vars, 'custom', false);
-
-            $env = getenv('ENV');
-            if (empty($env) || ($env !== PROD)) {
-                if ($env === 'PREPROD') {
-                    $dest = Config::$notificationAddressPreprod;
-                    $adminDest = Config::$notificationAddressPreprod;
-                } else {
-                    $dest = Config::$notificationAddressDev;
-                    $adminDest = Config::$notificationAddressDev;
-                }
-            }
-
-            /**
-             * wp_mail : envoie mail destinataire multiple
-             *
-             * @see wp-includes/pluggable.php : 228
-             * @param string|array $to Array or comma-separated list of email addresses to send message.
-             */
-            wp_mail($dest, Config::$mailSubjectFormationPreinscription. ' : ' .$session->formation->post->post_title, $message, $headers);
-            wp_mail($adminDest, Config::$mailSubjectAdminFormationPreinscription. ' : ' .$session->formation->post->post_title, $messageAdmin, $headers);
-
+            $this->_sendNotificationMail($data['dest'], $vars, Config::$mailSubjectFormationPreinscription. ' : ' .$session->formation->post->post_title);
+            $this->_sendAdminNotificationMail($data['adminDest'], $vars, Config::$mailSubjectAdminFormationPreinscription. ' : ' .$session->formation->post->post_title);
         }
     }
 
     public function sendEmailGenerique($formationThematique, $formationCommentaire) {
-        // en-tete email
-        $headers = array('Content-Type: text/html; charset=UTF-8');
-        $notaire = mvc_model('Notaire')->getUserConnectedData();
-
-        // list des notaires à notifier par etude
-        $notary = array($notaire);
-
-        $adminDest = Config::$notificationAddressFormulaireFormation;
-        $dest        = array();
-        if (is_array($notary) && count($notary) > 0) {
-            foreach ($notary as $item) {
-                if (filter_var($item->email_adress, FILTER_VALIDATE_EMAIL)) {
-                    $dest[] = $item->email_adress;
-                }
-            }
-        }
+        $data = $this->_prepareNotificationsMails();
 
         // destinataire non vide
-        if (count($dest) > 0) {
-            array_unique($dest);
+        if (count($data['dest']) > 0) {
+            array_unique($data['dest']);
             $vars    = array(
                 'type'                  => CONST_FORMATION_GENERIQUE,
                 'name'                  => $formationThematique,
                 'commentaire'           => $formationCommentaire,
-                'notaire'               => array(
-                    'crpcen'                => $notaire->crpcen,
-                    'fname'                 => $notaire->first_name,
-                    'lname'                 => $notaire->last_name,
-                    'mail'                  => !empty($notaire->email_adress) ? $notaire->email_adress : '',
-                ),
+                'notaire'               => $data['notaire']
             );
-            $message = CriRenderView('mail_notification_formation', $vars, 'custom', false);
-            $messageAdmin = CriRenderView('mail_notification_admin_formation', $vars, 'custom', false);
-
-            $env = getenv('ENV');
-            if (empty($env) || ($env !== PROD)) {
-                if ($env === 'PREPROD') {
-                    $dest = Config::$notificationAddressPreprod;
-                    $adminDest = Config::$notificationAddressPreprod;
-                } else {
-                    $dest = Config::$notificationAddressDev;
-                    $adminDest = Config::$notificationAddressDev;
-                }
-            }
-
-            /**
-             * wp_mail : envoie mail destinataire multiple
-             *
-             * @see wp-includes/pluggable.php : 228
-             * @param string|array $to Array or comma-separated list of email addresses to send message.
-             */
-            wp_mail($dest, Config::$mailSubjectFormationGenerique. ' : ' .$formationThematique, $message, $headers);
-            wp_mail($adminDest, Config::$mailSubjectFormationGenerique. ' : ' .$formationThematique, $messageAdmin, $headers);
+            $this->_sendNotificationMail($data['dest'], $vars, Config::$mailSubjectFormationGenerique. ' : ' .$formationThematique);
+            $this->_sendAdminNotificationMail($data['adminDest'], $vars, Config::$mailSubjectFormationGenerique. ' : ' .$formationThematique);
 
         }
     }
 
     public function sendEmailDemande ($formation, $formationParticipants, $formationCommentaire){
-        // en-tete email
-        $headers = array('Content-Type: text/html; charset=UTF-8');
+        $data = $this->_prepareNotificationsMails();
+
+        // destinataire non vide
+        if (count($data['dest']) > 0) {
+            array_unique($data['dest']);
+            $vars    = array(
+                'type'                  => CONST_FORMATION_DEMANDE,
+                'name'                  => $formation->post->post_title,
+                'participants'          => $formationParticipants,
+                'commentaire'           => $formationCommentaire,
+                'notaire'               => $data['notaire'],
+            );
+            $this->_sendNotificationMail($data['dest'], $vars, Config::$mailSubjectFormationDemande. ' : ' .$formation->post->post_title);
+            $this->_sendAdminNotificationMail($data['adminDest'], $vars, Config::$mailSubjectFormationDemande. ' : ' .$formation->post->post_title);
+        }
+    }
+
+    protected function _prepareNotificationsMails() {
         $notaire = mvc_model('Notaire')->getUserConnectedData();
 
         // list des notaires à notifier par etude
@@ -154,44 +89,38 @@ class Formation extends \App\Override\Model\CridonMvcModel
             }
         }
 
-        // destinataire non vide
-        if (count($dest) > 0) {
-            array_unique($dest);
-            $vars    = array(
-                'type'                  => CONST_FORMATION_DEMANDE,
-                'name'                  => $formation->post->post_title,
-                'participants'          => $formationParticipants,
-                'commentaire'           => $formationCommentaire,
-                'notaire'               => array(
-                    'crpcen'                => $notaire->crpcen,
-                    'fname'                 => $notaire->first_name,
-                    'lname'                 => $notaire->last_name,
-                    'mail'                  => !empty($notaire->email_adress) ? $notaire->email_adress : '',
-                ),
-            );
-            $message = CriRenderView('mail_notification_formation', $vars, 'custom', false);
-            $messageAdmin = CriRenderView('mail_notification_admin_formation', $vars, 'custom', false);
-
-            $env = getenv('ENV');
-            if (empty($env) || ($env !== PROD)) {
-                if ($env === 'PREPROD') {
-                    $dest = Config::$notificationAddressPreprod;
-                    $adminDest = Config::$notificationAddressPreprod;
-                } else {
-                    $dest = Config::$notificationAddressDev;
-                    $adminDest = Config::$notificationAddressDev;
-                }
+        $env = getenv('ENV');
+        if (empty($env) || ($env !== PROD)) {
+            if ($env === 'PREPROD') {
+                $dest = Config::$notificationAddressPreprod;
+                $adminDest = Config::$notificationAddressPreprod;
+            } else {
+                $dest = Config::$notificationAddressDev;
+                $adminDest = Config::$notificationAddressDev;
             }
-
-            /**
-             * wp_mail : envoie mail destinataire multiple
-             *
-             * @see wp-includes/pluggable.php : 228
-             * @param string|array $to Array or comma-separated list of email addresses to send message.
-             */
-            wp_mail($dest, Config::$mailSubjectFormationDemande. ' : ' .$formation->post->post_title, $message, $headers);
-            wp_mail($adminDest, Config::$mailSubjectFormationDemande. ' : ' .$formation->post->post_title, $messageAdmin, $headers);
-
         }
+
+        return array(
+            'dest' => $dest,
+            'adminDest' => $adminDest,
+            'notaire' => array(
+                'crpcen'                => $notaire->crpcen,
+                'fname'                 => $notaire->first_name,
+                'lname'                 => $notaire->last_name,
+                'mail'                  => !empty($notaire->email_adress) ? $notaire->email_adress : '',
+            ),
+        );
+    }
+
+    protected function _sendNotificationMail($dest, $vars, $subject) {
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+        $message = CriRenderView('mail_notification_formation', $vars, 'custom', false);
+        wp_mail($dest, $subject, $message, $headers);
+    }
+
+    protected function _sendAdminNotificationMail($dest, $vars, $subject) {
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+        $messageAdmin = CriRenderView('mail_notification_admin_formation', $vars, 'custom', false);
+        wp_mail($dest, $subject, $messageAdmin, $headers);
     }
 }
