479203a7ee75ab0139e543beff93ec5b03bac32a
Clément Horgues <clement.horgues@jetpulp.fr>
2017-03-22 16:48:19 +0100

    Readability checker js + base css WIP CSS

--- server/wp-content/themes/maestro/content-posez-question.php
+++ server/wp-content/themes/maestro/content-posez-question.php
@@ -156,7 +156,10 @@
 							<?php $icomp++; ?>
 						<?php endforeach; ?>
 						<input class="js-question-object" type="text" name="question_objet" id="question_objet" value="" placeholder="Objet de la question / Références de dossier" maxlength="80">
-						<textarea class="js-question-message" name="question_message" id="question_message" placeholder="Votre question"></textarea>
+                        <div class="js-question-readability question_readability hidden" >
+                            <span class="attention">Attention</span><span class="message">Pensez à aérer votre saisie.</span>
+                        </div>
+                        <textarea class="js-question-message" name="question_message" id="question_message" placeholder="Votre question"></textarea>
 
                         <?php for ($i = 0; $i < 5; $i++) : ?>
 						<div class="fileUpload btn btn-primary js-file-hide <?php echo ($i == 0) ? "" : "hidden"; ?>">
