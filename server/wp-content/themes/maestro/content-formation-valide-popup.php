2a846f83c3f3aa97f6b9ed25509340e915ea95d2
Amsellem <amsellem@mbp-de-renaud.add-online.add-online.loc>
2017-03-31 11:52:12 +0200

    Fix 's'

--- server/wp-content/themes/maestro/content-formation-valide-popup.php
+++ server/wp-content/themes/maestro/content-formation-valide-popup.php
@@ -13,9 +13,9 @@
 
     <div class="block">
         <?php if (!empty($preinscription)) : ?>
-            <?php _e('Votre demande de pré-inscription a bien été envoyée, vous serez recontactés par le CRIDON LYON prochainement'); ?>
+            <?php _e('Votre demande de pré-inscription a bien été envoyée, vous serez recontacté par le CRIDON LYON prochainement'); ?>
         <?php elseif (!empty($demandeFormation) || !empty($demandeGenerique)) : ?>
-            <?php _e('Votre demande de formation a bien été envoyée, vous serez recontactés par le CRIDON LYON prochainement'); ?>
+            <?php _e('Votre demande de formation a bien été envoyée, vous serez recontacté par le CRIDON LYON prochainement'); ?>
         <?php endif; ?>
         <br />
         <a href="<?php echo MvcRouter::public_url(array('controller' => 'formations', 'action' => 'calendar')) ?>" class="" >J'ai compris : retour au calendrier des formations</a>
